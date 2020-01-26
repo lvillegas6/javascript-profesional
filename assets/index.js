ed9995ad541bfa35a675607a6bf9cb66bc65844e
Luis Andres Villegas <luis.villegas@unisimon.edu.co>
2020-01-25 21:11:56 -0500

    intersectionObserver y visibilityChange

--- assets/index.js
+++ assets/index.js
@@ -1,13 +1,15 @@
 //Cuando usamos scrips con type="moduls" debemos ser especificos, declarar la extension.
 import MediaPlayer from './MediaPlayer.js'
 import AutoPlay from './plugins/AutoPlay.js'
+import AutoPause from './plugins/AutoPause.js'
 
 const video = document.querySelector('video');
 const player = new MediaPlayer({element: video, plugins: [ 
-    new AutoPlay()
+    new AutoPlay(),
+    new AutoPause()
 ] });
 
-const button = document.querySelector('#buttonPlay');
-const buttonMute = document.querySelector('#buttonMute');
+const button = document.querySelector('#playButton');
+const buttonMute = document.querySelector('#muteButton');
 button.onclick = () => player.togglePlay();
 buttonMute.onclick = () => player.toggleMute();
---
+++ assets/plugins/AutoPause.js
@@ -0,0 +1,43 @@
+class AutoPause {
+
+    constructor(){
+        this.handlerIntersection = this.handlerIntersection.bind(this) //Con bind le decimos quien va a ser el this (forzamos) a que siempre sea la clase.
+        this.handleVisibilityChange = this.handleVisibilityChange.bind(this)
+        this.threshold = 0.25
+    }
+
+    run(player) {
+
+        this.player = player
+                                                // El this se va a definir a base del objeto que esta llamando la funcion
+        const observer = new IntersectionObserver(this.handlerIntersection, {
+            threshold: this.threshold //este umbral lo que va a definir es: que porcentaje del elemento tiene que tener interccion con el contenedor para que te interese y me avise
+        });
+
+        observer.observe(this.player.media)
+
+        document.addEventListener('visibilitychange', this.handleVisibilityChange)
+
+    }
+
+    handlerIntersection(entries){
+
+        const entry = entries[0]
+
+        const isVisible = entry.intersectionRatio >= this.threshold
+
+        if(isVisible){
+            this.player.play()
+        }else{
+            this.player.pause();
+        }
+    }
+
+    handleVisibilityChange(){
+        const isVisible = document.visibilityState === 'visible'
+        
+        isVisible ? this.player.play() : this.player.pause()
+    }
+}
+
+export default AutoPause;
